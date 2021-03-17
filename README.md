3e73b794e07bf0c3527807ba2714d8911526996b
gox <gox.roche@gmail.com>
2021-03-17 17:38:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# factorio-blueprint
+# factorio-blueprint
---
+++ index.js
@@ -0,0 +1,31 @@
+const zlib = require("zlib"); 
+
+module.exports.blueprintDecode = (bpStr) => {
+    if(!bpStr || bpStr.length <= 1){
+        return null;
+    }
+    const bpDecoded = Buffer.from(bpStr.substring(1), 'base64');
+    const bpmDecodeInflated = zlib.inflateSync(bpDecoded);
+    return JSON.parse(bpmDecodeInflated.toString('utf-8'));
+}
+
+module.exports.blueprintEncode = (bpJson) => {
+    let deflated = zlib.deflateSync(JSON.stringify(bpJson), { level: 9 });
+    return "0" + deflated.toString('base64');
+}
+
+module.exports.listEntities = (bpStr) => {
+    const bpObj = this.blueprintDecode(bpStr);
+    if(!bpObj.blueprint || !bpObj.blueprint.entities){
+        return null;
+    }
+    let entities = {};
+    bpObj.blueprint.entities.forEach(entity => {
+        if(!entities[entity.name]){
+            entities[entity.name] = 1;
+        } else {
+            entities[entity.name] = entities[entity.name] + 1;
+        }
+    });
+    return entities;
+}
